1f340713ad81a40693e35f8709bd3749c5b0aa6f
Konstantin Senkin <kettariec@gmail.com>
2023-10-09 10:24:00 +0300

    task 1 done

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,24 @@
-"""Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Node, Stack
+
+
+class TestStack(unittest.TestCase):
+    """тест класса Node"""
+    def test_node(self):
+        node1 = Node(5, None)
+        node2 = Node('test', node1)
+        self.assertEqual(node1.data, 5)
+        self.assertEqual(node2.data, 'test')
+        self.assertEqual(node1.next_node, None)
+        self.assertEqual(node2.next_node, node1)
+
+    def test_stack_push(self):
+        """тест push класса Stack"""
+        stack = Stack()
+        stack.push('test1')
+        stack.push('test2')
+        stack.push('test3')
+        self.assertEqual(stack.top.data, 'test3')
+        self.assertEqual(stack.top.next_node.data, 'test2')
+        self.assertEqual(stack.top.next_node.next_node.data, 'test1')
+        self.assertEqual(stack.top.next_node.next_node.next_node, None)
