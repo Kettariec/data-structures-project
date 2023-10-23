b1d6f520e110438cc48799fff8541f60bc7aedc7
Konstantin Senkin <kettariec@gmail.com>
2023-10-23 13:26:56 +0300

    task 5 done

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,48 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+
+    def __init__(self, data, next_node=None):
+        """
+        Конструктор класса Node
+
+        :param data: данные, которые будут храниться в узле
+        """
+        self.data = data
+        self.next_node = next_node
 
 
 class LinkedList:
     """Класс для односвязного списка"""
 
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.data_list = []
+
     def insert_beginning(self, data: dict) -> None:
-        """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        """
+        Принимает данные (словарь) и добавляет узел
+        с этими данными в начало связанного списка
+        """
+        new_node = Node(data, self.head)
+        self.head = new_node
+        self.data_list.insert(0, data)
 
     def insert_at_end(self, data: dict) -> None:
-        """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        """
+        Принимает данные (словарь) и добавляет узел
+        с этими данными в конец связанного списка
+        """
+        if self.head is None:
+            self.head = Node(data)
+        else:
+            node = self.head
+            while node:
+                if node.next_node is None:
+                    node.next_node = Node(data)
+                    break
+                node = node.next_node
+        self.data_list.append(data)
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -22,8 +52,8 @@ class LinkedList:
 
         ll_string = ''
         while node:
-            ll_string += f' {str(node.data)} ->'
+            ll_string += f'{str(node.data)} -> '
             node = node.next_node
 
-        ll_string += ' None'
+        ll_string += 'None'
         return ll_string
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,34 @@
-"""Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import unittest
+from src.linked_list import Node, LinkedList
+
+
+class TestLinkedList(unittest.TestCase):
+    def test_node(self):
+        """тест класса Node"""
+        node1 = Node(5, None)
+        self.assertEqual(node1.data, 5)
+        self.assertEqual(node1.next_node, None)
+        self.assertIsInstance(node1, Node)
+        self.assertIsInstance(node1.data, int)
+
+    def test_insert_beginning(self):
+        """тесты класса LinkedList"""
+        new_list = LinkedList()
+        new_list.insert_beginning('data1')
+        new_list.insert_beginning('data2')
+        self.assertEqual(new_list.head.data, 'data2')
+        self.assertEqual(new_list.head.next_node.data, 'data1')
+
+    def test_insert_at_end(self):
+        new_list = LinkedList()
+        new_list.insert_at_end('data1')
+        new_list.insert_at_end('data2')
+        self.assertEqual(new_list.head.data, 'data1')
+        self.assertEqual(new_list.head.next_node.data, 'data2')
+
+    def test_str(self):
+        new_list = LinkedList()
+        self.assertEqual(new_list.__str__(), 'None')
+        new_list.insert_beginning('data1')
+        new_list.insert_beginning('data2')
+        self.assertEqual(new_list.__str__(), 'data2 -> data1 -> None')
